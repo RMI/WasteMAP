9e1ed96aba3bbf7de6385324b4e3c5c49fe628aa
andre-scheinwald <130578303+andre-scheinwald@users.noreply.github.com>
2023-05-08 10:02:52 -0600

    decision_support_v1
    first upload of decision support python script.  And some changes to the xlwings sandbox.

--- sandbox/xlwings_test.py
+++ sandbox/xlwings_test.py
@@ -3,10 +3,11 @@ import pandas as pd
 import numpy as np
 
 #Puth the local path to the RMI Sharepoint Documents folder here
-local_path = 'Testbook.xlsx'
+#local_path = 'Testbook.xlsx'
 #'/Users/atecza/Library/CloudStorage/OneDrive-RMI/Documents/'
 
-filepath_workbook = f'{local_path}WasteMap/Sandbox/Testbook'
+#filepath_workbook = f'{local_path}WasteMap/Sandbox/Testbook'
+filepath_workbook = 'C:\\Users\\andre.scheinwald\\Downloads\\Testbook.xlsx'
 
 #open workbook
 xl = xw.App(visible=False)
@@ -14,6 +15,8 @@ wb = xw.Book(filepath_workbook)
 
 #Practice retreaiving values
 my_value = wb.sheets["Inputs"].range("A2").value
+my_value_2 = wb.sheets["Inputs"].range("A2:A6").value
+my_value_3 = wb.sheets["Functions"].range("A2:B6").value
 print(my_value)
 
 #Play around here. Practice with setting and removing values
@@ -21,8 +24,22 @@ wb.sheets["Outputs"].range("A3").value = 100
 wb.sheets["Outputs"].range("A6").value = 200
 
 
+# Testing out a for loop to input a value down a column
+for i in range(1,10):
+    cell = f'A{i+1}'
+    print(cell)
+    wb.sheets["Retrieval"].range(cell).value = i
+    
+# Retrieving the results from the next column over
+my_value_4 = wb.sheets["Retrieval"].range("A1:B10").value
+
+df = pd.DataFrame(my_value_4)
+
+df = df.rename(columns=df.iloc[0]).drop(df.index[0]).reset_index(drop=True)
+
 
 # close the workbook
+wb.save()
 wb.close()
 xl.quit()
 del xl
