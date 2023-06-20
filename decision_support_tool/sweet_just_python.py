0f59f553a32f7e8f88e8020eed695167eaba1d4c
HughRunyan <hugh.runyan@gmail.com>
2023-06-20 13:17:33 -0700

    SAM little things and SWEET python only update a little

--- decision_support_tool/sweet_just_python.py
+++ decision_support_tool/sweet_just_python.py
@@ -232,26 +232,27 @@ region_lookup = {'China': 'Eastern Asia',
                 }
 
 
-msw_per_capita_defaults = {'Australia and New Zealand' : 1.644,
-                           'Caribbean' : 2.137,
-                           'Central America' : 1.589,
-                           'Eastern Africa' : .795,
-                            'Eastern Asia' : 1.315,
-                            'Eastern Europe' : 1.014,
-                            'Middle Africa' : .521,
-                            "North America" : 2.63,
-                            "Northern Africa" : 1.123,
-                            "Northern Europe" : 1.315,
-                            "Rest of Oceania" : .932,
-                            "South America" : 1.178,
-                            "Central Asia" : .932,
-                            "South-Eastern Asia" : 1.26,
-                            "Southern Africa" : .904,
-                            "Southern Europe" : 1.288,
-                            "Western Africa" : 0.493,
-                            "Western Asia" : 1.89,
-                            "Western Europe" : 1.616,
-                            "Southern Asia" : 1.37}
+msw_per_capita_defaults = {'Australia and New Zealand' : 1.643835616438360,
+                           'Caribbean' : 2.136986301369860,
+                           'Central America' : 1.589041095890410,
+                           'Central Asia' : 0.931506849315068,
+                           'Eastern Africa' : 0.794520547945205,
+                           'Eastern Asia' : 1.315068493150680,
+                           'Eastern Europe' : 1.013698630136990,
+                           'Middle Africa' : 0.520547945205479,
+                           'North America' : 2.630136986301370,
+                           "Northern Africa" : 1.123287671232880,
+                           "Northern Europe" : 1.315068493150680,
+                           "Rest of Oceania" : 0.931506849315068,
+                           "South America" : 1.178082191780820,
+                           "South-Eastern Asia" : 1.260273972602740,
+                           "Southern Africa" : 0.904109589041096,
+                           "Southern Asia" : 1.369863013698630,
+                           "Southern Europe" : 1.287671232876710,
+                           "Western Africa" : 0.493150684931507,
+                           "Western Asia" : 1.890410958904110,
+                           "Western Europe" : 1.616438356164380,
+                            }
 
 k_defaults = {'Dry':            {'food': .1,  'diapers': .1,  'green': .05, 'paper_cardboard': .02,  'textiles': .02,  'wood': .01,  'rubber': .01},
               'Moderately Dry': {'food': .18, 'diapers': .18, 'green': .09, 'paper_cardboard': .036, 'textiles': .036, 'wood': .018, 'rubber': .018},
@@ -278,14 +279,20 @@ def get_precipitation_zone(rainfall):
     
 L_0 = {'food': 70, 'diapers': 112, 'green': 93, 'paper_cardboard': 186, 'textiles': 112, 'wood': 200, 'rubber': 100}
 
+oxidation_factor = {'with_lfg':{'landfill': 0.22, 'controlled_dump_site': 0.1, 'dump_site': 0, 'remediated_to_landfill': 0.18}, 
+                    'without_lfg':{'landfill': 0.1, 'controlled_dump_site': 0.05, 'dump_site': 0, 'remediated_to_landfill': 0.1}}
+
+#mef_compost = 0.005876238822222 # Unit is Mg CO2e/Mg of organic waste, wtf
+mef_anaerobic = 0.000286598 # Unit is Mg CH4/Mg organic waste...wtf 
+
 #%%
 
 import pandas as pd
 import numpy as np
-import math
 
 runs_q = {}
 runs_m = {}
+organics_q = {}
 
 # Load cities
 cities = pd.read_csv('C:/Users/hughr/OneDrive/Documents/RMI/What_a_Waste/city_level_data_0_0.csv')
@@ -293,7 +300,7 @@ cities = pd.read_csv('C:/Users/hughr/OneDrive/Documents/RMI/What_a_Waste/city_le
 for row in cities.iterrows():
     city = row[1]['city_name']
     country = row[1]['country_name']
-    region = region_lookup[row[1]['country_name']]
+    region = region_lookup[country]
 
     # population
     population = float(row[1]['population_population_number_of_people'])
@@ -355,6 +362,9 @@ for row in cities.iterrows():
 
     waste_fractions = waste_fractions.to_dict()
 
+    mef_compost = (0.0055 * waste_fractions['food']/(waste_fractions['food'] + waste_fractions['green']) + \
+                   0.0139 * waste_fractions['green']/(waste_fractions['food'] + waste_fractions['green'])) * 1.1023 * 0.7 / 28
+                   # Unit is Mg CO2e/Mg of organic waste, wtf, so convert to CH4. Mistake in sweet here
     # precipitation
     precip = 1277.8 # mm
     precip_zone = get_precipitation_zone(precip)
@@ -376,16 +386,16 @@ for row in cities.iterrows():
     components = set(['food', 'green', 'wood', 'paper_cardboard', 'textiles'])
     
     # landfills
-    landfill_sites = ['Landfill', 'Dump']
-    mcfs = {'Landfill': mcf_landfill, 'Dump': mcf_dump}
-    diversion_fractions = {'Landfill': np.nan_to_num(row[1]['waste_treatment_controlled_landfill_percent'])/100,
-                            'Dump': (np.nan_to_num(row[1]['waste_treatment_open_dump_percent']) +
+    landfill_sites = ['landfill', 'dump_site']
+    mcfs = {'landfill': mcf_landfill, 'dump_site': mcf_dump}
+    diversion_fractions = {'landfill': np.nan_to_num(row[1]['waste_treatment_controlled_landfill_percent'])/100,
+                            'dump_site': (np.nan_to_num(row[1]['waste_treatment_open_dump_percent']) +
                                     np.nan_to_num(row[1]['waste_treatment_landfill_unspecified_percent']) +
                                     np.nan_to_num(row[1]['waste_treatment_unaccounted_for_percent']))/100}
     
     diversion_total = sum([diversion_fractions[x] for x in diversion_fractions.keys()])
     if diversion_total == 0:
-        diversion_fractions = {'Landfill': 0, 'Dump': 1}
+        diversion_fractions = {'landfill': 0, 'dump_site': 1}
     else:
         for site in diversion_fractions.keys():
             diversion_fractions[site] /= diversion_total
@@ -394,7 +404,7 @@ for row in cities.iterrows():
 
     # compost
     compost_total = 0
-    compost_components = set(['food', 'green', 'wood']) # Double check we don't want to include paper
+    compost_components = set(['food', 'green', 'wood', 'paper_cardboard']) # Double check we don't want to include paper
     compost_fraction_of_total = float(row[1]['waste_treatment_compost_percent']) / 100   
     if compost_fraction_of_total != compost_fraction_of_total:
         compost_fraction_of_total = 0
@@ -420,7 +430,7 @@ for row in cities.iterrows():
 
     # anaerobic digestion
     anaerobic_total = 0
-    components_anaerobic = set(['food', 'green', 'wood'])
+    components_anaerobic = set(['food', 'green', 'wood', 'paper_cardboard'])
     anaerobic_fraction_of_total = float(row[1]['waste_treatment_anaerobic_digestion_percent']) / 100
     if anaerobic_fraction_of_total != anaerobic_fraction_of_total:
         anaerobic_fraction_of_total = 0
@@ -476,10 +486,11 @@ for row in cities.iterrows():
 
     q = {}
     ms = {}
+    mass_compost = {}
+    mass_anaerobic = {}
     q_df = {}
     ms_df = {}
-
-    # Currently the non 100% split between landfills is a problem. 
+    organic_df = {}
 
     for site in landfill_sites:
         mcf = mcfs[site]
@@ -490,6 +501,9 @@ for row in cities.iterrows():
         for waste in components:
             ms[mcf][waste] = []
             q[mcf][waste] = {}
+            mass_compost[waste] = []
+            mass_anaerobic[waste] = []
+            organic_df[waste] = {}
             for t in range(50):
                 #if (waste in compost_components) and (waste != 'paper_cardboard'):
                     # ms[mcf][waste].append((waste_total * (1.03 ** t) * waste_fractions[waste] - 
@@ -505,18 +519,24 @@ for row in cities.iterrows():
                                         combustion_vol[waste] - 
                                         recycling_vol[waste]) 
                                         * diversion_fractions[site] * (1.03 ** t))
+                # Only have to do these once
+                if site == 'landfill':
+                    mass_compost[waste].append(compost_vol[waste] * (1.03 ** t))
+                    mass_anaerobic[waste].append(anaerobic_vol[waste] * (1.03 ** t))
                 # else:
                 #     ms[mcf][waste].append(site_waste * (1.03 ** t) * waste_fractions[waste] - 
                 #                           combustion_vol[waste] * diversion_fractions[site] * (1.03 ** t) - 
                 #                           recycling_vol[waste] * diversion_fractions[site] * (1.03 ** t))
                 ch4 = []
                 for y in range(t):
-                    val = ks[waste] * L_0[waste] * ms[mcf][waste][y] * np.exp(-ks[waste] * (t - y - 0.5)) * mcf
+                    val = ks[waste] * L_0[waste] * ms[mcf][waste][y] * np.exp(-ks[waste] * (t - y - 0.5)) * mcf * (1 - oxidation_factor['without_lfg'][site])
                     ch4.append(val)
                 #Q3[waste][t] = convert_methane_m3_to_ton(sum(ch4)) * 28
                 # if sum(ch4) == 0.0:
                 #     break
-                q[mcf][waste][t] = sum(ch4) #* .9
+                q[mcf][waste][t] = sum(ch4)
+                if site == 'landfill':
+                    organic_df[waste][t] = mass_compost[waste][t] * mef_compost + mass_anaerobic[waste][t] * mef_anaerobic
                 #Ms2[waste] = [x * np.exp(-k[waste]) for x in Ms2[waste]]
                 
         
@@ -526,37 +546,28 @@ for row in cities.iterrows():
 
     runs_q[city] = q_df
     runs_m[city] = ms_df
+    organics_q[city] = pd.DataFrame(organic_df)
 
     if country == 'Argentina':
         break
+    
+# REMEMBER TO ADD THE UNSPECIFIED LANDFILL COLUMN
 
-# %%
+#%%
+def convert_methane_m3_to_ton_co2e(volume_m3):
+    density_kg_per_m3 = 0.7168
+    mass_kg = volume_m3 * density_kg_per_m3
+    mass_ton = mass_kg / 1000
+    mass_co2e = mass_ton * 28
+    return mass_co2e
 
-for i in diversion_fractions.keys():
-    print(i, waste_total * diversion_fractions[i])
-
-waste_total / population * 1000 / 365
-sum([waste_total * diversion_fractions[x] for x in diversion_fractions.keys()]) / population * 1000 / 365
-
-print(compost_fraction_of_total)
-print(anaerobic_fraction_of_total)
-print(combustion_fraction_of_total)
-
-print(compost_total)
-print(anaerobic_total)
-print(combustion_total)
-
-{x: waste_fractions[x] / fraction_recyclable for x in components_recycling}
-sum([waste_fractions[x] / fraction_recyclable for x in components_recycling])
-
-3323.398
-
-# %%
-runs_m[city][1]
-runs_m[city][.4]
-runs_q[city][1]
-runs_q[city][.4]
-
-#runs_q[city][1] + runs_q[city][.4]
-runs_m[city][1] + runs_m[city][.4]
-# %%
+runs_q_tons = {}
+for city in runs_q.keys():
+    run = runs_q[city]
+    runs_q_tons[city] = {}
+    for mcf in run.keys():
+        site = run[mcf]
+        runs_q_tons[city][mcf] = convert_methane_m3_to_ton_co2e(site)
+        
+        
+#units_df = convert_methane_m3_to_ton(q_df[1]) * 28
